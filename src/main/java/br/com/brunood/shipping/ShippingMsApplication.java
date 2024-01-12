13759f2fa77697f15ccea1e8a995666efce87fb2
brunood07 <brunood07@gmail.com>
2024-01-12 08:25:12 -0300

    refactor: adjusts for container running

--- src/main/java/br/com/brunood/shipping/ShippingMsApplication.java
+++ src/main/java/br/com/brunood/shipping/ShippingMsApplication.java
@@ -2,8 +2,9 @@ package br.com.brunood.shipping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ShippingMsApplication {
 
 	public static void main(String[] args) {
